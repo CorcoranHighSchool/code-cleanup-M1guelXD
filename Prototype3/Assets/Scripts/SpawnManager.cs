3eb4205a0b12640abe24df089f1c89661a77388d
M1guelXD <142622731+M1guelXD@users.noreply.github.com>
2024-04-11 12:49:52 -0700

    Removed unused libraries. removed unused methods. added SerializeField added string const.cs

--- Prototype3/Assets/Scripts/SpawnManager.cs
+++ Prototype3/Assets/Scripts/SpawnManager.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     //Prefab for the obstacle to spawn
-    public GameObject obstaclePrefab;
+    [SerializeField] private GameObject obstaclePrefab;
     //Spawn Position
     private Vector3 spawnPos = new Vector3(25.0f, 0.0f, 0.0f);
 
@@ -18,12 +16,6 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         InvokeRepeating("SpawnObstacle", spawnDelay, spawnRate);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void SpawnObstacle()
